f25599789733c82c79a244248268a9e802183b1d
Kit Fuderich <kitfuderich@gmail.com>
2022-01-30 12:19:33 -0800

    input form and button added and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField'
 
-let name: string;
-let age: number | string;
-let isstudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-//tuple
-role = [5, "string"];
-//objects
-type Person = {
-  name: string;
-  age?: number;
-}
-let person: Person;
-age = 5
-age = "happy"
-person = { name: "Kit" }
-
-//array of objects
-let lotsofPerson: Person[];
-
-function printName(name: string) {
-  console.log(name)
-}
-
-printName("hello")
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello
+      <span className="heading">Taskify</span>
+      <InputField></InputField>
     </div>
   );
 }
