04fd6021e4ce0d360ad87ce86b6d23d3d159dd9e
Kit Fuderich <kitfuderich@gmail.com>
2022-01-30 12:50:56 -0800

    todo added to todo list with use state and handle add

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,12 +4,15 @@ import './styles.css'
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
     return (
-        <form className='input'>
-            <input type="input" placeholder="enter a task" className="input__box" />
+        <form className='input' onSubmit={handleAdd}>
+            <input type="input" placeholder="enter a task" className="input__box"
+                onChange={(e) => setTodo(e.target.value)}
+            />
             <button className='input_submit' type='submit'>
                 Go
             </button>
